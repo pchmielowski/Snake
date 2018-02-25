84e15737255f18d46ce307156a80cc7350d6c039
Piotrek Chmielowski <piotrek.chmielowski@callpage.io>
2018-02-25 21:26:44 +0100

    Method removed

--- app/Main.hs
+++ app/Main.hs
@@ -88,8 +88,6 @@ loop w state = do
                     }
                else (resetTimer now) {snake = init $ newHead : snake state}
     hitsItself = any (samePosition newHead) (tail (snake state))
-    -- TODO: rename
-    theSamePositionAsNewHead = samePosition newHead
     eatsMeal = samePosition (head (snake state)) (meal state)
     samePosition (Vector ax ay) (Vector bx by) = ax == bx && ay == by
     nextGenerator = (snd . next) $ generator state
