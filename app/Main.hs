b11210c4bc96062f333e6cfd67ec5cbc7dcff350
Piotrek Chmielowski <piotrek.chmielowski@callpage.io>
2018-02-25 15:44:07 +0100

    Simple timer

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,44 @@
 module Main where
 
-import UI.NCurses
+import Control.Monad.IO.Class
 import Data.Time.Clock.POSIX
+import UI.NCurses
 
 import Lib
 
+-- milliseconds
+frameDelay = 123
+
+currentTime = fmap round $ fmap (* 1000) getPOSIXTime
+
 main :: IO ()
-main =
-  runCurses $ do
-    setEcho False
-    w <- defaultWindow
-    runGame w
+main = do
+  t <- currentTime
+  loop 0 t
+
+loop delta before = do
+  now <- currentTime
+  if (delta > frameDelay)
+    then do
+      putStrLn $ show delta
+      loop 0 now
+    else loop (delta + (now - before)) now
+
+-- main :: IO ()
+-- main =
+--   runCurses $ do
+--     setEcho False
+--     w <- defaultWindow
+--     t <- liftIO $ currentTime
+--     runGame w
+data State = State
+  { time :: Integer
+  }
 
 runGame w = loop
   where
     loop = do
+      now <- liftIO $ round `fmap` getPOSIXTime
       updateScreen
       ev <- getEvent w Nothing
       case ev of
